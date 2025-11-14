9ff15a85eab34dfecca27bf77c2562d1274462bc
Aabhas Nama <aabhasa9@gmail.com>
2025-11-14 17:15:20 +0530

    added chat code

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const chatController = require("../controllers/chat.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.post("/", chatController.createChat);
+router.post("/", authMiddleware.authUser, chatController.createChat);
 router.get("/");
 router.get("/messages/:id");
